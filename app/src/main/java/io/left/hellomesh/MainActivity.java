732486a29905ceb331352eb0b56b85d5cb709290
anthony <anthonycsli123@gmail.com>
2018-01-13 18:43:38 -0800

    private connection

--- app/src/main/java/io/left/hellomesh/MainActivity.java
+++ app/src/main/java/io/left/hellomesh/MainActivity.java
@@ -45,8 +45,8 @@ public class MainActivity extends Activity implements MeshStateListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mm = AndroidMeshManager.getInstance(MainActivity.this, MainActivity.this);
+        mm.setPattern("chatroom");
     }
 
     /**
